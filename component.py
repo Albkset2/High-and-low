b4c2cf657f0f82946111f7b1279513956e6773eb
Albkset <83570540+Albkset@users.noreply.github.com>
2021-08-30 11:53:31 +1200

    i have done the impossible

--- component.py
+++ component.py
@@ -1,18 +1,151 @@
-def modes(question, answer, error):
-    valid = False
-    while not valid:
-        response = input(question).strip().lower()
-        for item in answer:
-            if response == item[0] or response == item:
-                answer = item
-                print("program continues")
+
+from portals3 import SLEEP_BETWEEN_ACTIONS
+import random
+import time
+import os
+os.system("color")
+
+players = 0
+SLEEP_BETWEEN_ACTIONS = 0.5
+board = [
+    ["100", " 99", " 98", " 97", " 96", " 95", " 94", " 93", " 92", " 91"],
+    [" 81", " 82", " 83", " 84", " 85", " 86", " 87", " 88", " 89", " 90"],
+    [" 80", " 79", " 78", " 77", " 76", " 75", " 74", " 73", " 72", " 71"],
+    [" 61", " 62", " 63", " 64", " 65", " 66", " 67", " 68", " 69", " 70"],
+    [" 60", " 59", " 58", " 57", " 56", " 55", " 54", " 53", " 52", " 51"],
+    [" 41", " 42", " 43", " 44", " 45", " 46", " 47", " 48", " 49", " 50"],
+    [" 40", " 39", " 38", " 37", " 36", " 35", " 34", " 33", " 32", " 31"],
+    [" 21", " 22", " 23", " 24", " 25", " 26", " 27", " 28", " 29", " 30"],
+    [" 20", " 19", " 18", " 17", " 16", " 15", " 14", " 13", " 12", " 11"],
+    ["  1", "  2", "  3", "  4", "  5", "  6", "  7", "  8", "  9", " 10"],
+]
+
+black_hole = {
+    8: 4,
+    18: 1,
+    26: 10,
+    39: 5,
+    51: 6,
+    54: 36,
+    56: 1,
+    60: 23,
+    75: 28,
+    83: 45,
+    85: 59,
+    90: 48,
+    92: 25,
+    97: 87,
+    99: 63
+}
+portal = {
+    3: 20,
+    6: 14,
+    11: 28,
+    15: 34,
+    17: 74,
+    22: 37,
+    38: 59,
+    49: 67,
+    57: 76,
+    61: 78,
+    73: 86,
+    81: 98,
+    88: 91
+}
+
+
+class Colors:
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    INVERTED = '\033[7m'
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    BRIGHT_BLACK = '\033[30;1m'
+    BRIGHT_RED = '\033[31;1m'
+    BRIGHT_GREEN = '\033[32;1m'
+    BRIGHT_YELLOW = '\033[33;1m'
+    BRIGHT_BLUE = '\033[34;1m'
+    BRIGHT_MAGENTA = '\033[35;1m'
+    BRIGHT_CYAN = '\033[36;1m'
+    BRIGHT_WHITE = '\033[37;1m'
+    BACKGROUND_BLACK = '\033[40m'
+    BACKGROUND_RED = '\033[41m'
+    BACKGROUND_GREEN = '\033[42m'
+    BACKGROUND_YELLOW = '\033[43m'
+    BACKGROUND_BLUE = '\033[44m'
+    BACKGROUND_MAGENTA = '\033[45m'
+    BACKGROUND_CYAN = '\033[46m'
+    BACKGROUND_WHITE = '\033[47m'
+    BACKGROUND_BRIGHT_BLACK = '\033[40;1m'
+    BACKGROUND_BRIGHT_RED = '\033[41;1m'
+    BACKGROUND_BRIGHT_GREEN = '\033[42;1m'
+    BACKGROUND_BRIGHT_YELLOW = '\033[43;1m'
+    BACKGROUND_BRIGHT_BLUE = '\033[44;1m'
+    BACKGROUND_BRIGHT_MAGENTA = '\033[45;1m'
+    BACKGROUND_BRIGHT_CYAN = '\033[46;1m'
+    BACKGROUND_BRIGHT_WHITE = '\033[47;1m'
+
+
+def move(old_value):
+    return old_value + random.choice([1, 2, 3, 4, 5, 6])
+
+
+black_hole_text = [
+    "sad",
+    "oh no",
+    "too bad",
+    "better luck next time",
+    "You have been sucked back",
+    "dang",
+    "boohoo",
+    "bummer"
+]
+portals_text = [
+    "woww",
+    "yaayyy"
+    "nailed it",
+    "woohoo",
+    "oh my God...",
+    "up and away",
+    "this never gets old"
+
+]
+
+
+def draw_board(board, players) -> None:
+    
+    for y in board:
+        for _ in y:
+            print("------", end="")
+        print("-\n|", end="")
+
+        for piece in y:
+            found = False
+            for player in players:
+                if str(player) == piece.strip():
+                    print(f" {Colors.BACKGROUND_BRIGHT_GREEN}{piece}{Colors.RESET} |", end="")
+                    found = True
+                    break
                 
                 
-        # output error if item not in list
-        print(error)
+            if not found:
+                if black_hole.get(int(piece.strip())) != None:
+                    print(
+                        f" {Colors.BACKGROUND_BRIGHT_RED}{piece}{Colors.RESET} |", end="")
+                elif portal.get(int(piece.strip())) != None:
+                    print(
+                        f" {Colors.BACKGROUND_BRIGHT_BLUE}{piece}{Colors.RESET} |", end="")
+                else:
+                    print(f" {piece} |", end="")
         print()
 
-mode = ["solo", "duo","multi", "xxx"]
-
-hello = modes("What mode would you like to play \nAvailable options:  Solo, Duo and Multi \n : ",
-              mode, "Please enter the available modes")
+    for _ in board[0]:
+        print("------", end="")
+    print("-")
